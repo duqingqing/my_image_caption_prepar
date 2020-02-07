2b9ea2f98bf1aea1350c1801d1f299f4cecc63b7
duqingqing <dulovefighting@sina.com>
2020-02-07 17:40:19 +0800

    优化网络

--- faster_rcnn_video_detection/video_detection.py
+++ faster_rcnn_video_detection/video_detection.py
@@ -5,7 +5,7 @@ import cv2
 import sys
 
 sys.path.append('./')
-from faster_rcnn_detection_on_vedio import coco_names
+from faster_rcnn_video_detection import coco_names
 import random
 
 def get_args():
@@ -26,29 +26,30 @@ def random_color():
 
 
 def img_detection(src_img): #src_img from cv2.imread()
-    args = get_args()
-    input = []
-    num_classes = 91
-    names = coco_names.names
-    model = torchvision.models.detection.__dict__[args.model](num_classes=num_classes, pretrained=True)
-    model = model.cuda()
-    model.eval()
-    # src_img = cv2.imread(image_path)
-    img = cv2.cvtColor(src_img, cv2.COLOR_BGR2RGB)
-    img_tensor = torch.from_numpy(img / 255.).permute(2, 0, 1).float().cuda()
-    input.append(img_tensor)
-    out = model(input)
-    boxes = out[0]['boxes']
-    labels = out[0]['labels']
-    scores = out[0]['scores']
-    for idx in range(boxes.shape[0]):
-        if scores[idx] >= args.score:
-            x1, y1, x2, y2 = boxes[idx][0], boxes[idx][1], boxes[idx][2], boxes[idx][3]
-            name = names.get(str(labels[idx].item()))
-            cv2.rectangle(src_img, (x1, y1), (x2, y2), random_color(), thickness=2)
-            cv2.putText(src_img, text=name, org=(x1, y1 + 10), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-                        fontScale=1, thickness=2, lineType=cv2.LINE_AA, color=(0, 0, 255))
-    return src_img
+    with torch.no_grad():
+        args = get_args()
+        input = []
+        num_classes = 91
+        names = coco_names.names
+        model = torchvision.models.detection.__dict__[args.model](num_classes=num_classes, pretrained=True)
+        model = model.cuda()
+        model.eval()
+        # src_img = cv2.imread(image_path)
+        img = cv2.cvtColor(src_img, cv2.COLOR_BGR2RGB)
+        img_tensor = torch.from_numpy(img / 255.).permute(2, 0, 1).float().cuda()
+        input.append(img_tensor)
+        out = model(input)
+        boxes = out[0]['boxes']
+        labels = out[0]['labels']
+        scores = out[0]['scores']
+        for idx in range(boxes.shape[0]):
+            if scores[idx] >= args.score:
+                x1, y1, x2, y2 = boxes[idx][0], boxes[idx][1], boxes[idx][2], boxes[idx][3]
+                name = names.get(str(labels[idx].item()))
+                cv2.rectangle(src_img, (x1, y1), (x2, y2), random_color(), thickness=2)
+                cv2.putText(src_img, text=name, org=(x1, y1 + 10), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                            fontScale=1, thickness=2, lineType=cv2.LINE_AA, color=(0, 0, 255))
+        return src_img
 
 def video_detection(video_path): #video_path = 0 means read cv2.VideoCapture(0)
     cam = cv2.VideoCapture(video_path)  # /home/dqq/下载/mz.mp4
@@ -65,4 +66,4 @@ def video_detection(video_path): #video_path = 0 means read cv2.VideoCapture(0)
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
-    video_detection("/home/dqq/视频/video.mp4")
+    video_detection("/home/dqq/视频/test.mp4")
